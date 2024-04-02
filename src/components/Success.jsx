84787568892f68700abc5fde3377ad6ee0442c6a
milanthapaliya <thapaliyamilan8@gmail.com>
2024-04-02 16:37:38 +0545

    updated success page

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,8 +1,15 @@
+import {useNavigate} from "react-router-dom";
 
 const Success = ({ message }) => {
+    const navigate = useNavigate();
+
+    const goToHome = () => {
+        navigate('/');
+    };
     return (
         <div className="success-container">
             <div className="success-message">Code Verified Successfully</div>
+            <button className="home-button" onClick={goToHome}>Go to Home</button>
         </div>
     );
 };
